6715de72c2bd89fb675acd56fb6cfc0477a88c88
drpradsms <dr.pradsms@gmail.com>
2018-08-22 09:49:26 +0000

    Build oppo

--- common.mk
+++ common.mk
@@ -479,7 +479,7 @@ PRODUCT_COPY_FILES += \
 
 # Inherit from oppo-common
 
-#$(call inherit-product, device/oppo/common/common.mk)
+$(call inherit-product, device/oppo/common/common.mk)
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
