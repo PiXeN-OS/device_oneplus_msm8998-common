0544e6154decf042089800bd7c7fd7b302f85c1d
drpradsms <dr.pradsms@gmail.com>
2018-08-25 17:28:23 +0000

    Add Volte prop and build Ipacm

--- system_prop.mk
+++ system_prop.mk
@@ -227,3 +227,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+#VoLTE
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
