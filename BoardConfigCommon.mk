45a86d5283b8db7290ad2b7b7f9c21342b3278db
drpradsms <dr.pradsms@gmail.com>
2018-09-18 06:11:19 +0000

    Update clang to 7.0.2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,7 +84,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := redflare-custom_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := 6.0.2
+TARGET_KERNEL_CLANG_VERSION := 7.0.2
 TARGET_USE_SDCLANG := true
 
 # Enable real time lockscreen charging current values
