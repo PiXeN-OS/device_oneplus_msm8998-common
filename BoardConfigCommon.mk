5e9c8c4255f2db6f9b0a033c191c128e868e073d
drpradsms <dr.pradsms@gmail.com>
2018-09-29 07:59:03 +0000

    Revert "Build permissive and remove all SEPolicy for now"
    This reverts commit 29b42fa35479f6fe6c9330983b05004d6c38e6da.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,7 +71,6 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048
 BOARD_KERNEL_CMDLINE += androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.wificountrycode=US
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
@@ -300,8 +299,8 @@ PROTOBUF_SUPPORTED := true
 BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 
 # SELinux
-#include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Timeservice
 BOARD_USES_QC_TIME_SERVICES := true
