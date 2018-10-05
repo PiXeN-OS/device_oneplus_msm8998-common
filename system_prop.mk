8c379901725f3197c335d2103d0738daf135ecc0
Pranav Vashi <neobuddy89@gmail.com>
2018-10-05 19:38:07 +0530

    msm8998-common: Add support for OP cam flashable
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -70,10 +70,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.oneplus.camera,com.android.camera,org.lineageos.snap \
-    camera.hal1.packagelist=org.thunderdog.challegram,com.skype.raider,com.instagram.android,com.whatsapp \
+    camera.hal1.packagelist=com.skype.raider,com.instagram.android,com.whatsapp,com.oneplus.camera \
     persist.camera.mobicat=2 \
     persist.camera.stats.debugexif=3080192 \
-    persist.ts.rtmakeup=false
+    persist.ts.rtmakeup=false \
+    camera.disable_zsl_mode=true \
+    persist.sys.camera.preview.size=1920x1080
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
