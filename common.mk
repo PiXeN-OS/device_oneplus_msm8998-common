c0c368b668f865f39dfd3bae4b68636482c24420
drpradsms <dr.pradsms@gmail.com>
2018-10-20 04:50:43 +0000

    Pixel powerhal 1/2
    ref: https://github.com/mTresk/android_device_oneplus_oneplus5/tree/android-8.1/power
    Signed-off-by: drpradsms <dr.pradsms@gmail.com>

--- common.mk
+++ common.mk
@@ -360,7 +360,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+   android.hardware.power@1.1-service.oneplus5
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
